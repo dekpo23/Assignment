8498abb7844f05f13b2eea723a9a74404f389d75
David Ekpo <jofesdavid@gmail.com>
2025-08-19 02:56:11 -0700

    Task 8 mod

--- Week 2/Task8/Question5.py
+++ Week 2/Task8/Question5.py
@@ -1,5 +1,12 @@
+#Create Dictionary
 store = {'Book': 10, 'Pen': 20, 'Bag': 5}
+#Create Dictionary's copy
+store_copy = store.copy()
+#Update Dictionary
 item_to_buy = input('Enter item you want to buy: ')
 item_quantity = int(input('Enter quantity you want to buy: '))
 store[item_to_buy] -= item_quantity
-print(store)
+
+#Print Output
+print(f'Original Dictionary: {store_copy}')
+print(f'Dictionary After: {store}')
--- Week 2/Task8/Question6.py
+++ Week 2/Task8/Question6.py
@@ -1,8 +1,13 @@
+#Collect student info
 age = int(input('Enter your age here: '))
 utme_score = int(input('Enter your utme score: '))
 olevel = input('Do you have 5 credits in your olevel: ')
 postutme = input('Did you paticipate in post utme?(True or False) ')
+dept_score = float(input('Enter cumulative score after post utme here: '))
 dept_cut_off = float(input('Enter departmental cut off here: '))
 
-final_admission = (age >= 16) and (utme_score >= 200) and (olevel.lower() == 'true') and (postutme.lower() == 'true') and (dept_cut_off >= 200)
+#Check admission status
+final_admission = (age >= 16) and (utme_score >= 200) and (olevel.lower() == 'true') and (postutme.lower() == 'true') and (dept_score >= dept_cut_off)
+
+#Print final message
 print(f'Admitted: {final_admission}')
